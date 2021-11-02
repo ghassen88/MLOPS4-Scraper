c4efde2de31638f4dd5d61fc5694a9822288ae49
ghassen88 <boussaaghassen2015@gmail.com>
2021-11-02 12:08:39 +0100

    Second commit

--- src/main/java/com/codetriage/scraper/App.java
+++ src/main/java/com/codetriage/scraper/App.java
@@ -18,6 +18,9 @@ public class App {
    */
   public static void main(String[] args) {
 
+
+    System.out.println("hello word !!!");
+
     try {
       /**
        * Here we create a document object,
